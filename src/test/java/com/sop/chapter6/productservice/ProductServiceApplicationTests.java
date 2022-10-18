2665bce3fcbb3f670b150fc2f0525c1730854b30
Tiwat Parkpoompaisal <ttimetoworkk@gmail.com>
2022-10-18 15:27:56 +0700

    Initial commit

---
+++ src/test/java/com/sop/chapter6/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.chapter6.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
